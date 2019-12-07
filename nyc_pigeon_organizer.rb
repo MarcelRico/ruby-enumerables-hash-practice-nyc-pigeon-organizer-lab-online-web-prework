e598b9591a9fea156fe6e62952414a5a925ad4a4
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 13:03:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,14 @@
 def nyc_pigeon_organizer(data)
   
+  result = {}
+  
   data[:gender].map do |array|
     key = array[0].to_s
     name_array = array[1]
     
     name_array.length.times do |name_idx|
       name = value_array[name_idx]
-      
+      result[name] = key
     end
     
     puts key
