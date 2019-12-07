a5ea6d9761fe76bf7a7ac60e49f8fab5d99ed00a
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 12:42:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,5 @@
 def nyc_pigeon_organizer(data)
-  pigeons = {}
-  
-  idx=1
-  data[:color].map do |name|
-    color = name[0]
-    name.length.times do |idx|
-      puts name[idx]
-    end
-    idx+=1
-  end
-  
-  pp pigeons
+  pp data[:color]
 end
 
 #Example of data
