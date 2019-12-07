7a7af0ea8b0554229577d2a142cd5cdb3354f894
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 13:16:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,17 @@
 def nyc_pigeon_organizer(data)
-  
-end
 
-def restructure_data()
+  result_hash = {}
+
+  attribute = :gender
+  
+  data[attribute].map do |attribute_arr|
+    key = attribute_arr[0].to_s
+    values = attribute_arr[1]
+    values.length.times do |value_idx|
+      if(result_hash[values[value_idx]])
+    end
+  end
+
+
+
+end
