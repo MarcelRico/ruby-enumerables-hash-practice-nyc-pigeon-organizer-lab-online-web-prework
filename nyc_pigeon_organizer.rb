05ac45cf19743e5c41071c327090cacd7bed7b84
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 13:07:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,19 @@ def nyc_pigeon_organizer(data)
   
   result = {}
   
-[].length.times do |validates_each|
-  data[attribute].map do |array|
-    key = array[0].to_s
-    name_array = array[1]
-    
-    name_array.length.times do |name_idx|
-      name = name_array[name_idx]
-      result[name] = {attribute=>key}
+  [:gender,:color,:lives].each do |attribute_idx|
+    attribute = attribute_idx
+    data[attribute].map do |array|
+      key = array[0].to_s
+      name_array = array[1]
+      
+      name_array.length.times do |name_idx|
+        name = name_array[name_idx]
+        result[name] = {attribute=>key}
+      end
+      
+      puts key
+      pp result
     end
-    
-    puts key
-    pp result
   end
-  
 end
