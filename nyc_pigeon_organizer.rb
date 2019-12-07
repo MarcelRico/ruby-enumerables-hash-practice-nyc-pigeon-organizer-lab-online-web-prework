2ac939d0fd044262fd6aa369829eaa895472a91e
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 13:33:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,14 @@
 def nyc_pigeon_organizer(data)
-  
+  pigeon_names = get_pigeon_names(data)
+  create_result_hash(pigeon_names,data)
 end
 
 def create_result_hash(pigeon_names,attributes)
   result_hash = {}
   pigeon_names.map do |name|
+    result_hash[name] = []
     attributes.map do |attribute|
-      result_hash[name] 
+      result_hash[name] << {attribute=>""}
     end
   end
   result_hash
