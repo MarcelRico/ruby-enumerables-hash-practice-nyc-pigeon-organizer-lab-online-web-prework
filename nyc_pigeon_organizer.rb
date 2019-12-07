d33c9785bd96f33bd3e2b5f92fdedd6cdcab1c81
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 14:10:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,20 +6,21 @@ def nyc_pigeon_organizer(data)
     attr_values_line = attribute_line[1] # Example: {:gender =>{:male=>[name1, name2,name3],:female=>[name1,name2,name3]}
     
     attr_values_line.map do |values_line| # Example: {:male=>[name1,name2]}
-      attribute_value = values_line[0].to_s # :
-      pigeon_names = values_line[1]
+      attribute_value = values_line[0].to_s # Example: "male"
+      pigeon_names = values_line[1] # An array containing the pigeon names from the current attribute values line
      
      
       pigeon_names.map do |pigeon_name|
       
-        if(pigeon_list[pigeon_name]==nil)
+        if(pigeon_list[pigeon_name]==nil) # If no hash entry exists for the pigeon name, create one
           pigeon_list[pigeon_name] = {}
         end
         
-        if(pigeon_list[pigeon_name][attribute]==nil)
+        if(pigeon_list[pigeon_name][attribute]==nil) # If no Array entry exists for pigeon's attribute, create one
           pigeon_list[pigeon_name][attribute] = []
         end
-          
+        
+        # push the current attribute value onto the array that exists for the pigeons attribute. Example: 
         pigeon_list[pigeon_name][attribute] << attribute_value
      
       end
