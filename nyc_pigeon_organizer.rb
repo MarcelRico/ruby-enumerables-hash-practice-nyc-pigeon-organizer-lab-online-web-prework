00257cb6bc6f6e3ff782521280fb66772ca1f234
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 13:26:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,9 +3,7 @@ def nyc_pigeon_organizer(data)
 
   result_hash = restructure_pigeon_data(data,:gender,result_hash)
 
-  result_hash = restructure_pigeon_data(data,:lives,result_hash)
-
-
+  restructure_pigeon_data(data,:lives,result_hash)
 
   pp result_hash
 end
