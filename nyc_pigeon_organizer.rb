8f5f65c369dae5f0f896a34fd502aaeb42a0a127
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 13:55:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,10 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   
   data.map do |attribute_line| # Array of attribute lines (attr and values)
-    pp attribute_line
+    attribute = attribute_line[0]
+    attr_values_line = attribute_line[1]
+    
+    
   end
   
   
