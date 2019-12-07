d994210ea2582243e627f96dcf8f4065a9352fb7
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 12:52:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,7 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
   
-  data[:color].map do |array| 
-    color = array[0].to_s
-    array[1].length.times do |idx|
-      name = array[1][idx]
-      pigeons[name] = {:color => color}
-    end
-  end
+  func(data,attribute)
   
   
   
@@ -19,3 +13,14 @@ end
 #Example of data
 #{:pigeon_name=>{:color=>"",:gender=>"",:lives=>""}}
 
+def func(data,attribute)
+  result = {}
+  data[attribute].map do |array| 
+    color = array[0].to_s
+    array[1].length.times do |idx|
+      name = array[1][idx]
+      pigeons[name] = {attribute => color}
+    end
+  end
+  return 
+end
