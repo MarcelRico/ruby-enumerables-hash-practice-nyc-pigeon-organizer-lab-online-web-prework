9a740e4eab9110b25777cb761ef4d06bebfa9e31
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 13:58:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,6 +8,7 @@ def nyc_pigeon_organizer(data)
     attr_values_line.map do |values_line|
       color = values_line[0]
       pigeon_names = values_line[1]
+      pp pigeon_names
     end
   end
   
