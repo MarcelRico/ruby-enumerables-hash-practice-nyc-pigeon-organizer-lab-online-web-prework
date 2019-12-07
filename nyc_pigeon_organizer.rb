9a13837bb55e86896c0c2326bb628b1261b9bb3d
Marcel Rico <personal.mr4@gmail.com>
2019-12-07 13:27:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,17 +9,3 @@ def nyc_pigeon_organizer(data)
 end
 
 
-def restructure_pigeon_data(data,attribute,result_hash)
-  data[attribute].map do |attribute_arr|
-    key = attribute_arr[0].to_s
-    pigeon_names = attribute_arr[1]
-    pigeon_names.length.times do |pigeon_idx|
-      name = pigeon_names[pigeon_idx]
-      if(result_hash[name] == nil)
-        result_hash[name] = []
-      end
-      result_hash[name] << {attribute=>key}
-    end
-  end
-  result_hash
-end
